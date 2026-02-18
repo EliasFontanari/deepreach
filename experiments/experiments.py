55e3466043cd9eab0e87a961842952aac72cd9a5
EliasFontanari <elias.fontanari@unitn.it>
2026-02-18 17:21:27 +0100

    Dynamics with new systems added

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -74,7 +74,10 @@ class Experiment(ABC):
                 
                 ax = fig.add_subplot(len(times), len(zs), (j+1) + i*len(zs))
                 ax.set_title('t = %0.2f, %s = %0.2f' % (times[i], plot_config['state_labels'][plot_config['z_axis_idx']], zs[j]))
-                s = ax.imshow(1*(values.detach().cpu().numpy().reshape(x_resolution, y_resolution).T <= 0), cmap='bwr', origin='lower', extent=(-1., 1., -1., 1.))
+                ax.set_xlabel(f"{plot_config['state_labels'][plot_config['x_axis_idx']]}")
+                ax.set_ylabel(f"{plot_config['state_labels'][plot_config['y_axis_idx']]}")
+                # s = ax.imshow(1*(values.detach().cpu().numpy().reshape(x_resolution, y_resolution).T <= 0), cmap='bwr', origin='lower', extent=(-1., 1., -1., 1.))
+                s = ax.imshow(values.detach().cpu().numpy().reshape(x_resolution, y_resolution).T, cmap='bwr', origin='lower', extent=(-1., 1., -1., 1.))
                 fig.colorbar(s) 
         fig.savefig(save_path)
         if self.use_wandb:
